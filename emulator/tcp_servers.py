71fcfa8009075618e35d402a0938b220ec6f95af
Takefumi MIYOSHI <miyo@wasamon.net>
2021-05-13 20:31:38 +0900

    add required command to run examples/bram_send_recv

--- emulator/tcp_servers.py
+++ emulator/tcp_servers.py
@@ -18,27 +18,52 @@ def start_server(server, body):
     body(client)
 
 def ctrl_handler(client):
+    global ctrl_client
     buffer_size = 1024
+    bitstream = 0
     while True:
         data = client.recv(buffer_size)
         print("[CTRL] Received Data: {}".format(data))
-        if data == b'Version\r\n':
-            print("send verstion")
-            client.send(b" 1.4\r\n")
-        elif data == b'disconnect\r\n':
-            print("disconnect")
-            client.send(b" 1.4\r\n")
+        s = data.decode()
+        if s.startswith("disconnect"):
+            client.send(b' 1.4\r\n')
             return
+        elif s.startswith("Version"):
+            client.send(b' 1.4\r\n')
+        elif s.startswith("GetBitstream"):
+            client.send(b' 1 1\r\n')
+        elif s.startswith("SetBitstream"):
+            values = s.split(" ")
+            bitstream = int(values[1])
+            client.send(b' 1 1\r\n')
+        elif s.startswith("GetTriggerStatus"):
+            client.send(b' GetTriggerStatus 0 \r\n')
+        elif s.startswith("GetIntrStatus"):
+            client.send(b' GetIntrStatus 0 0 0 0\r\n')
         else:
-            client.send(b"\r\n")
+            client.send(b' unknown \r\n')
         
 def data_handler(client):
-    buffer_size = 1024
+    buffer_size = 65536*2
     while True:
         data = client.recv(buffer_size)
-        print("[CTRL] Received Data: {}".format(data))
-        if data == b'':
-            return
+        print("[DATA] Received Data: {}".format(data))
+        try:
+            if data == b'':
+                return
+            s = data.decode()
+            if s.startswith('ReadDataFromMemory'):
+                values = s.split(" ")
+                reply_len = int(values[3])
+                reply_dat = b'0' * reply_len
+                print(reply_dat)
+                client.send(reply_dat + b'\r\n')
+                client.send(b' ReadDataFromMemory \r\n')
+        except UnicodeDecodeError:
+            if data == b'':
+                return
+            else:
+                client.send(b' data recv \r\n')
 
 ctrl_server = init_server(8081)
 ctrl_thread = threading.Thread(target=start_server, args=(ctrl_server,ctrl_handler))
